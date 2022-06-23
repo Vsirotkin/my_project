8a023ee95b1fa2e8eb7ca778ab0c920093fe85f9
Vsirotkin <vsirotkin15@gmail.com>
2022-06-23 10:25:27 +0300

    initial commit

---
+++ src/pages/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request):
+    return render(request, 'home.html', {})
+
+my_list = [1, 2, 'word from my list']
+def about_view(request):
+    context = {
+        'my_list': my_list
+    }
+    return render(request, "about.html", context)
