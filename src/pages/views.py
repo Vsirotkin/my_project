4b54ba7fc47618c127475b0c8c3cfe911673386e
Viktor <vsirotkin15@gmail.com>
2022-06-23 18:50:38 +0300

    checkig usability in view - works

--- src/pages/views.py
+++ src/pages/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 def home_view(request):
     return render(request, 'home.html', {})
 
-my_list = [1, 2, 'word from my list']
+my_list = ['name', 'age', 'password']
 def about_view(request):
     context = {
         'my_list': my_list
