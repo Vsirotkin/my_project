903c6bae50b4f3cba57b89e3c4cdad9b3a0af271
Viktor <vsirotkin15@gmail.com>
2022-06-26 09:41:47 +0300

    made object - universal

--- src/products/views.py
+++ src/products/views.py
@@ -6,8 +6,14 @@ from .models import Product
 
 def product_detail_view(request):
     obj = get_object_or_404(Product, pk=1)
+    '''
     context = {
         'title': obj.title,
         'description': obj.description,
     }
+    '''
+    context = {
+        'object': obj,
+    }
+
     return render(request, 'product/detail_view.html', context)
