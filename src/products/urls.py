e5ce33fed21db6b57ecc92b7bd94b1611ee72b9c
Viktor <vsirotkin15@gmail.com>
2022-06-26 15:12:17 +0300

    added filling in form from site and got a blank if saved

--- src/products/urls.py
+++ src/products/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_detail_view, name='product_detail_view'),
+    path('', views.product_create_view, name='create_view'),
+    # path('product/', views.product_detail_view, name='product_detail_view'),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .forms import ProductForm
+# from .models import Product
 
 
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'products/product_create.html', context)
+
+
+'''
 def product_detail_view(request):
     obj = get_object_or_404(Product, pk=1)
 
@@ -10,3 +24,4 @@ def product_detail_view(request):
     }
 
     return render(request, 'products/product_detail_view.html', context)
+'''
