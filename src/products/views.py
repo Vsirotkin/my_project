76d04fcbf0ac0456af9451dcd53f38b7138c88f3
Vsirotkin <vsirotkin15@gmail.com>
2022-06-27 13:25:45 +0300

    just tries to with get and post

--- src/products/views.py
+++ src/products/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .forms import ProductForm
 # from .models import Product
 
+def product_create_view(request):
+    context = {}
 
+    return render(request, 'products/product_create.html', context)
+
+'''
 def product_create_view(request):
     form = ProductForm(request.POST or None)
     if form.is_valid():
@@ -15,7 +20,6 @@ def product_create_view(request):
     return render(request, 'products/product_create.html', context)
 
 
-'''
 def product_detail_view(request):
     obj = get_object_or_404(Product, pk=1)
 
